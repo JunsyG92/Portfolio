e5c8432b91a95e2723dbe5e64ac8c8b5be87cde6
JunsyG92 <mathieu.nesseir@gmail.com>
2020-11-12 21:24:38 +0100

    Initialized Vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-Vue.config.productionTip = false
+// import "bootstrap/scss/bootstrap.scss";
+import vuetify from "./plugins/vuetify";
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+	router,
+	vuetify,
+	render: (h) => h(App),
+}).$mount("#app");
