0b670453c90234fb619729c51b227b5c868d8d92
JunsyG92 <mathieu.nesseir@gmail.com>
2020-11-15 23:28:12 +0100

    Added axios in project's config

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import axios from "axios";
 
 // import "bootstrap/scss/bootstrap.scss";
 import vuetify from "./plugins/vuetify";
 
+axios.defaults.baseURL = "http://localhost:3000/api/";
+const token = sessionStorage.getItem("token");
+
+if (token) {
+	axios.defaults.headers.common["Authorization"] = "Bearer " + token;
+}
+
+// Prototypes
+Vue.prototype.$axios = axios;
 Vue.config.productionTip = false;
 
 new Vue({
