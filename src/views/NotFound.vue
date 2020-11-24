3049d83ef229f8d4c254c79bc6758702a1660bd4
JunsyG92 <mathieu.nesseir@gmail.com>
2020-11-24 22:55:39 +0100

    Modified design of NotFound page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,17 +1,35 @@
 <template>
-	<v-container class="notfound d-flex justify-center align-center h-100">
-		<div>
-			<h2 class="_title big _neon _blue">404</h2>
-			<h1 class="_title medium">Page not found.</h1>
-			<router-link to="/" class="d-block text-white item nav-link nav-hover"
-				>Go home</router-link
-			>
-		</div>
-	</v-container>
+	<div class="h-100">
+		<Menu />
+		<v-container class="notfound d-flex justify-center align-center h-100">
+			<v-row>
+				<v-col cols="12" class="d-flex">
+					<div class="mx-auto">
+						<h2 class="_title big _neon _blue">404</h2>
+						<h1 class="_title medium">Page not found.</h1>
+						<router-link
+							to="/"
+							class="d-block text-white item nav-link nav-hover"
+						>
+							<v-btn color="primary" outlined x-large block>
+								Go Home
+							</v-btn>
+						</router-link>
+					</div>
+				</v-col>
+			</v-row>
+		</v-container>
+	</div>
 </template>
 
 <script>
-export default {};
+// @ is an alias to /src
+import Menu from "../components/Header/Menu/Menu";
+export default {
+	name: "NotFound",
+	components: {
+		Menu,
+	},
+};
 </script>
-
 <style lang="scss" scoped></style>
