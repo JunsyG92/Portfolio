c9a84b72e2dc32e9a0cd921da330b91b5a749248
JunsyG92 <mathieu.nesseir@gmail.com>
2020-11-24 23:03:01 +0100

    Update of the login page and the login function

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,29 +1,36 @@
 <template>
 	<v-container class="login d-flex align-center h-100">
 		<v-row>
-			<v-col cols="4" class="mx-auto">
-				<form method="post">
-					<v-text-field v-model="email" type="email" label="E-mail" required />
-					<v-text-field
-						v-model="password"
-						type="password"
-						label="Password"
-						required
-					/>
+			<v-col cols="12" class="d-flex flex-column">
+				<div class="mx-auto">
+					<form method="post">
+						<v-text-field
+							v-model="email"
+							type="email"
+							label="E-mail"
+							required
+						/>
+						<v-text-field
+							v-model="password"
+							type="password"
+							label="Password"
+							required
+						/>
 
-					<v-btn
-						:loading="loading"
-						color="primary"
-						outlined
-						x-large
-						block
-						@click="Login"
-					>
-						Submit
-					</v-btn>
-				</form>
+						<v-btn
+							:loading="loading"
+							color="primary"
+							outlined
+							x-large
+							block
+							@click="Login"
+						>
+							Submit
+						</v-btn>
+					</form>
+				</div>
 				<v-alert
-					class="mt-5"
+					class="mt-5 mx-auto"
 					v-if="message"
 					color="red"
 					dense
@@ -59,13 +66,16 @@ export default {
 					password: this.password,
 				})
 				.then((res) => {
-					var status = res.status;
-					var message = res.data.erreur;
-					status ? (this.message = message) : "";
-					console.log(res);
-					localStorage.setItem("token", res.data.token);
-					this.loading = !this.loading;
-					// window.location.reload();
+					if (res.data.erreur) {
+						var status = res.status;
+						var message = res.data.erreur;
+						status ? (this.message = message) : "";
+						this.loading = !this.loading;
+					} else {
+						localStorage.setItem("token", res.data.token);
+						this.loading = !this.loading;
+						window.location.reload();
+					}
 				})
 				.catch((err) => {
 					// console.log(err);
