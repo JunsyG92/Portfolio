b84513e807ca756f70b0266ecfe3318d2e4fca20
JunsyG92 <mathieu.nesseir@gmail.com>
2020-11-24 23:04:52 +0100

    Initialized Vuex

---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+	state: {
+		chart: [
+			{ name: "HTML5", level: 90 },
+			{ name: "CSS3", level: 90 },
+			{ name: "JavaScript", level: 75 },
+		],
+	},
+	mutations: {
+		CHANGE_CHART_DATA(state, payload) {
+			state.chart = payload;
+		},
+	},
+	actions: {},
+	modules: {},
+});
