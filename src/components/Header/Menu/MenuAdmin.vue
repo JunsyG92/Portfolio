52f7707e3893b7c4eaed701f3542c0ffc8d409b9
JunsyG92 <mathieu.nesseir@gmail.com>
2020-11-24 23:06:33 +0100

    Created Menu's component for Admin page

---
+++ src/components/Header/Menu/MenuAdmin.vue
@@ -0,0 +1,22 @@
+<template>
+	<header class="pannel__admin_header">
+		<div class="pannel__admin_header_content"></div>
+	</header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.pannel__admin_header {
+	height: 100%;
+	width: 300px;
+}
+.pannel__admin_header_content {
+	width: 100%;
+	height: 100%;
+	background: transparent;
+	box-shadow: 0 0 20px #000;
+}
+</style>
