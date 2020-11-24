63d8f3503eb8d0da261c7944c3620955495a31b5
JunsyG92 <mathieu.nesseir@gmail.com>
2020-11-24 22:54:25 +0100

    Removed css to App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 	<v-app>
 		<v-main>
 			<Menu />
-			<!-- <Reseau /> -->
 			<router-view />
 		</v-main>
 	</v-app>
@@ -13,12 +12,9 @@
 import Menu from "./components/Header/Menu/Menu";
 export default {
 	name: "App",
-
 	components: {
-		// Reseau,
 		Menu,
 	},
-
 	data: () => ({
 		//
 	}),
@@ -45,23 +41,20 @@ export default {
 .scopeone {
 	font-family: "Scope One", sans-serif;
 }
-h1,
-h2,
-h3,
-h4,
-h5,
-h6 {
-	/* font-family: "Heavitas", sans-serif; */
-	/* src: url("./assets/fonts/Heavitas.ttf"); */
-}
-
-.theme--dark.v-application {
-	min-height: 100%;
+html {
 	/* background: rgba(50,50,58,1); */
+	// background: #fff !important;
 	background: #251b2e !important;
 	background: -moz-linear-gradient(top, #251b2e 0%, #06001e 100%) !important;
 	background: -webkit-linear-gradient(top, #251b2e 0%, #06001e 100%) !important;
 	background: linear-gradient(to bottom, #251b2e 0%, #06001e 100%) !important;
 	filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#251b2e', endColorstr='#06001e',GradientType=0 ) !important;
+}
+.container {
+	padding-left: 0 !important;
+	padding-right: 0 !important;
+}
+.theme--dark.v-application {
+	background: transparent !important;
 }
 </style>
