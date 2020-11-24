c467ca9c106d24aa89dd4293ed0feada71bea116
JunsyG92 <mathieu.nesseir@gmail.com>
2020-11-24 22:56:46 +0100

    Modified design of Admin page and get token

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,11 +1,40 @@
 <template>
-	<div>
-		<h1>Admin Panel</h1>
-	</div>
+	<section class="pannel">
+		<div class="pannel__admin">
+			<MenuAdmin />
+			<div class="pannel__content">
+				<h1>Admin Panel</h1>
+			</div>
+		</div>
+	</section>
 </template>
 
 <script>
-export default {};
+import MenuAdmin from "../components/Header/Menu/MenuAdmin";
+// let token = sessionStorage.getItem("token");
+export default {
+	components: {
+		MenuAdmin,
+	},
+	data() {
+		return {};
+	},
+	created() {
+		// let token = sessionStorage.getItem("token");
+	},
+};
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.pannel {
+	display: initial;
+}
+.pannel__admin {
+	width: 100%;
+	height: 100%;
+	display: flex;
+}
+.pannel__content {
+	padding: 20px 35px;
+}
+</style>
